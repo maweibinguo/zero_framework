a575037ef15e907cc289619f7e8a8f47903ce8f5
zero <maweibinguo@163.com>
2017-08-28 13:35:46 +0800

    Delete config.php

--- app/blog/config/develop/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'upload_file' => [
-                        'allow_type'        => ["image/jpg", "image/jpeg", "image/gif", "image/png", "mage/bmp", "image/webp"],
-                        'allow_max_size'    => 2 * 1024 * 1024 * 1024
-                     ]
-];
